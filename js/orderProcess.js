2b4f545b1b985b0ce87445870315bf57879c6108
earhater <tem100502@mail.ru>
2022-12-04 18:38:28 +0300

    added tg support. again( and again(

--- js/orderProcess.js
+++ js/orderProcess.js
@@ -1,9 +1,15 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
+
 let subnne = document.getElementById("subnn");
 
 subnne.addEventListener("click", function(){
-  const item = document.getElementsByTagName("input")[0].value
-  console.log("hello")
-  tg.sendData(item);
-});
+  //const item = document.getElementsByTagName("input")[0].value
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Вы выбрали товар 6!");
+    item = "6";
+    tg.MainButton.show();
+  }});
