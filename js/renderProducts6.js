93c79dbde561b0515e35848c20e28f084f704548
EARHAT <Tem100502@mail.ru>
2022-12-15 20:02:36 +0300

    finalization??

--- js/renderProducts6.js
+++ js/renderProducts6.js
@@ -17,37 +17,41 @@ function renderProducts123(productsArray) {
     productsArray.forEach(function (item) {
         const productHTML = `<div class="col-md-6">
 
-						<div class="card mb-4" data-id="${item.id}">
-							<img class="product-img" src="img/roll/${item.imgSrc}" alt="">
-							<div class="card-body text-center">
-								<h4 class="item-title">${item.title}</h4>
-								<div class='subscr'>
-								<p>${item.subcription}</p></div>
-								<p><small data-items-in-box class="text-muted">${item.itemsInBox} шт.</small></p>
+		<div class="card mb-4" data-id="${item.id}">
+		<h4 class="item-title">${item.title}</h4>
+			<img class="product-img" src="img/roll/${item.imgSrc}" alt="">
+			<div class="card-body text-center">
+				
+				<div class='subscr'>
+				<p>${item.subcription}</p></div>
+				
+				<div class="price">
+						
+						
+						<div class="inbox"><p>В наличии: ${item.itemsInBox}</p></div>
+						<div class="price__weight">${item.weight}г.</div>
+						<div class="price__currency">${item.price} ₽</div>
+				</div>
+				
+				<div class="details-wrapper">
 
-								<div class="details-wrapper">
+					<!-- Счетчик -->
+					<div class="items_counters">
+						<div class="items__control" data-action="minus">-</div>
+						<div class="items__current" data-counter>1</div>
+						<div class="items__control" data-action="plus">+</div>
+					</div>
+					<button  type="button" class="card-button">В корзину</button>
+					<!-- // Счетчик -->
 
-									<!-- Счетчик -->
-									<div class="items counter-wrapper">
-										<div class="items__control" data-action="minus">-</div>
-										<div class="items__current" data-counter>1</div>
-										<div class="items__control" data-action="plus">+</div>
-									</div>
-									<!-- // Счетчик -->
+					
+				</div>
 
-									<div class="price">
-										<div class="price__weight">${item.weight}г.</div>
-										<div class="price__currency">${item.price} ₽</div>
-									</div>
-								</div>
+				
 
-								<button data-cart type="button" class="btn btn-block btn-outline-warning">
-									+ в корзину
-								</button>
-
-							</div>
-						</div>
-					</div>`;
+			</div>
+		</div>
+	</div>`;
         productsContainer12.insertAdjacentHTML('beforeend', productHTML);
     });
 }
