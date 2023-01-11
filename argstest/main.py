68f397e42e38dbecc64675e1e881c9c46ec6f21b
earhater <109986940+earhater@users.noreply.github.com>
2023-01-11 19:44:55 +0300

    refferal test

--- argstest/main.py
+++ argstest/main.py
@@ -1,9 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import WebAppInfo
+
+
+
 import config as cfg
 import markups
 import markups as nav
+import pyqrcode as qr
 from db import Database
-
+import png
 bot = Bot(token=cfg.TOKEN)
 dp = Dispatcher(bot)
 db = Database("database")
@@ -25,6 +30,9 @@ async def start(message: types.Message):
                 except:
                     print("unsent")
                     pass
+
+
+
             else:
                 print("else1")
 
@@ -38,7 +46,23 @@ async def start(message: types.Message):
 @dp.message_handler()
 async def bot_message(message: types.Message):
     if message.text == "Профиль":
-        await bot.send_message(message.from_user.id, f"ID: {message.from_user.id} \nhttps://t.me/{cfg.BOT_NICKNAME}?start={message.from_user.id} рефералы:0")
+        await bot.send_message(message.from_user.id, f"ID: {message.from_user.id} \nhttps://t.me/{cfg.BOT_NICKNAME}?start={message.from_user.id} рефералы:0\n Ниже находится ваш персоальный qr код")
+        qrcode = qr.create(f"https://t.me/{cfg.BOT_NICKNAME}?start={message.from_user.id}")
+        qrcode.png("1.png", scale=6)
+
+
+        with open('1.png', 'rb') as photo:
+            await bot.send_photo(message.chat.id, photo)
+
+@dp.message_handler(content_types='web_app_data')
+async def buy_process(web_app_message):
+
+    mess = web_app_message.web_app_data.data
+    await bot.send_message(web_app_message.chat.id, mess)
+    requests.post(f"https://api.telegram.org/bot5960316813:AAE-dt5FKOP1xnsMOmvsludK9AjqDfiSF00/sendMessage?chat_id=-841404134&text={mess}")
+
+
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
--- argstest/markups.py
+++ argstest/markups.py
@@ -1,5 +1,14 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+from aiogram.types import WebAppInfo
+
+
+web_app_gov = WebAppInfo(url="https://earhater.github.io/")
+
+
+cc = KeyboardButton("Нажмите для входа в магазин", web_app=web_app_gov)
+
+
 mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 btnProfile = KeyboardButton("Профиль")
-mainMenu.add(btnProfile)
+mainMenu.add(btnProfile, cc)
